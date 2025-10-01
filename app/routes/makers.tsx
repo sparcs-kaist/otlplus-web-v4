1820044cd3d493045225f78f4328b64607828ff8
mbp16 <phw1508@naver.com>
2025-10-01 17:10:04 +0900

    feat: change default index

--- app/routes/makers.tsx
+++ app/routes/makers.tsx
@@ -7,6 +7,7 @@ import MemberSection from "@/features/makers/sections/MemberSection"
 import ProjectSection from "@/features/makers/sections/ProjectSection"
 import { MakersList } from "@/utils/makersList"
 
+
 const MakersWrapper = styled(FlexWrapper)`
   flex: 1 0 0;
   min-height: 0;
@@ -14,7 +15,7 @@ const MakersWrapper = styled(FlexWrapper)`
 `
 
 export default function MakersPage() {
-  const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(9)
 
   return (
     <MakersWrapper direction="column" align="center" justify="stretch" gap={36}>
