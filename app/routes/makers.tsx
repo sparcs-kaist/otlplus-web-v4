85aa481edd654cfaa9c9196084d774da6d5e242b
mbp16 <phw1508@naver.com>
2025-11-01 04:05:46 +0900

    fix: fix scroll issue

--- app/routes/makers.tsx
+++ app/routes/makers.tsx
@@ -8,22 +8,22 @@ import ProjectSection from "@/features/makers/sections/ProjectSection"
 import { MakersList } from "@/utils/makersList"
 
 const MakersWrapper = styled(FlexWrapper)`
-  flex: 1 0 0;
-  min-height: 0;
-  padding: 32px 16px;
+    flex: 1 0 auto;
+    min-height: 0;
+    padding: 32px 16px;
 `
 
 export default function MakersPage() {
-  const [currentIndex, setCurrentIndex] = useState(10)
+    const [currentIndex, setCurrentIndex] = useState(10)
 
-  return (
-    <MakersWrapper direction="column" align="center" justify="stretch" gap={36}>
-      <ProjectSection
-        makersList={MakersList}
-        currentIndex={currentIndex}
-        setCurrentIndex={setCurrentIndex}
-      />
-      <MemberSection membersList={MakersList[currentIndex].fields} />
-    </MakersWrapper>
-  )
+    return (
+        <MakersWrapper direction="column" align="center" justify="stretch" gap={36}>
+            <ProjectSection
+                makersList={MakersList}
+                currentIndex={currentIndex}
+                setCurrentIndex={setCurrentIndex}
+            />
+            <MemberSection membersList={MakersList[currentIndex].fields} />
+        </MakersWrapper>
+    )
 }
