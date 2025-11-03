fc22274f3b2575971f971fbdf0dc13475e282aff
mbp16 <phw1508@naver.com>
2025-11-03 20:15:34 +0900

    fix: i18n bug

--- app/common/components/Credits.tsx
+++ app/common/components/Credits.tsx
@@ -6,44 +6,44 @@ import FlexWrapper from "@/common/primitives/FlexWrapper"
 import Typography from "@/common/primitives/Typography"
 
 const StyledLink = styled(Link)`
-  text-decoration: none;
+    text-decoration: none;
 `
 
 const Credits: React.FC = () => {
-  const { t } = useTranslation()
+    const { t } = useTranslation()
 
-  return (
-    <FlexWrapper direction="column" gap={12} align={"center"} justify={"center"}>
-      <Typography color={"Text.placeholder"} type={"Bigger"}>
-        OTL PLUS
-      </Typography>
-      <FlexWrapper direction="row" gap={12} justify={"space-between"}>
-        <StyledLink to={"/makers"}>
-          <Typography color={"Text.placeholder"} type={"Big"}>
-            {t("credits.makers")}
-          </Typography>
-        </StyledLink>
-        <Typography color={"Text.placeholder"} type={"Big"}>
-          |
-        </Typography>
-        <Typography color={"Text.placeholder"} type={"Big"}>
-          {t("credits.license")}
-        </Typography>
-        <Typography color={"Text.placeholder"} type={"Big"}>
-          |
-        </Typography>
-        <Typography color={"Text.placeholder"} type={"Big"}>
-          {t("credits.privacyPolicy")}
-        </Typography>
-      </FlexWrapper>
-      <Typography color={"Text.placeholder"} type={"Big"}>
-        otlplus@sparcs.org
-      </Typography>
-      <Typography color={"Text.placeholder"} type={"Big"}>
-        Ⓒ 2016, SPARCS OTL TEAM
-      </Typography>
-    </FlexWrapper>
-  )
+    return (
+        <FlexWrapper direction="column" gap={12} align={"center"} justify={"center"}>
+            <Typography color={"Text.placeholder"} type={"Bigger"}>
+                OTL PLUS
+            </Typography>
+            <FlexWrapper direction="row" gap={12} justify={"space-between"}>
+                <StyledLink to={"/makers"}>
+                    <Typography color={"Text.placeholder"} type={"Big"}>
+                        {t("credits.credits")}
+                    </Typography>
+                </StyledLink>
+                <Typography color={"Text.placeholder"} type={"Big"}>
+                    |
+                </Typography>
+                <Typography color={"Text.placeholder"} type={"Big"}>
+                    {t("credits.license")}
+                </Typography>
+                <Typography color={"Text.placeholder"} type={"Big"}>
+                    |
+                </Typography>
+                <Typography color={"Text.placeholder"} type={"Big"}>
+                    {t("credits.privacyPolicy")}
+                </Typography>
+            </FlexWrapper>
+            <Typography color={"Text.placeholder"} type={"Big"}>
+                otlplus@sparcs.org
+            </Typography>
+            <Typography color={"Text.placeholder"} type={"Big"}>
+                Ⓒ 2016, SPARCS OTL TEAM
+            </Typography>
+        </FlexWrapper>
+    )
 }
 
 export default Credits
