0c5821a8252da2d3c46ee2bc06976f9e01b180b5
Hyunwoo Park <phw1508@naver.com>
2025-11-03 19:21:58 +0900

    Feature/makers (#10)
    * feat: add makers page
    
    * fix: remove PM style
    
    * feat: change default index
    
    * feat: change default index
    
    * feat: modify makers list, add hovering feature
    
    * fix: modify project page responsive design
    
    * fix: fix scroll issue
    
    * style: change font and color
    
    * style: add shadow

---
+++ app/features/makers/sections/MemberSection/index.tsx
@@ -0,0 +1,47 @@
+import styled from "@emotion/styled"
+
+import Typography from "@/common/primitives/Typography"
+import MemberCard from "@/features/makers/components/MemberCard"
+import type { MemberField } from "@/utils/makersList"
+
+const MemberSessionWrapper = styled.div`
+  max-width: 1280px;
+  width: 100%;
+`
+
+const MemberCardWrapper = styled.div`
+  display: grid;
+  width: 100%;
+  gap: 15px;
+  justify-content: center;
+  margin: 36px 0 0 0;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+`
+
+interface MemberSectionProps {
+  membersList: MemberField[]
+}
+
+const MemberSection: React.FC<MemberSectionProps> = ({ membersList }) => {
+  return (
+    <MemberSessionWrapper>
+      <Typography type="BiggerBold" color="Text.default">
+        Members
+      </Typography>
+      <MemberCardWrapper>
+        {membersList.map((field, index) =>
+          field.members.map((member) => (
+            <MemberCard
+              name={member.name}
+              nickname={member.nickname}
+              role={field.role}
+              hover={member.hover}
+            />
+          )),
+        )}
+      </MemberCardWrapper>
+    </MemberSessionWrapper>
+  )
+}
+
+export default MemberSection
