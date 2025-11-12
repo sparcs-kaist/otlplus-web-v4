df51691da00582e06f28509bb2f843fc6f7a31ce
yaong12 <yaong@sparcs.org>
2025-11-12 23:34:38 +0900

    useAPI options update

--- app/utils/api/useAPI.tsx
+++ app/utils/api/useAPI.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 
 import { useQuery } from "@tanstack/react-query"
+import { type AxiosHeaders } from "axios"
 import { useTranslation } from "react-i18next"
 
 import { axiosClient as axios } from "@/libs/axios"
@@ -12,14 +13,22 @@ import {
     type getAPIResponseType,
 } from "./getAPIType"
 
+type UseAPIOptions = {
+    headers?: AxiosHeaders
+    enabled?: boolean
+    staleTime?: number
+    gcTime?: number
+}
+
 export function useAPI<
     M extends Method<P>,
     P extends Path,
     Req extends getAPIRequestType<M, P>,
     Res extends getAPIResponseType<M, P>,
->(method: M, path: P, initialParams: Req = null as Req) {
-    const [params, setParams] = useState<Req>(initialParams)
+>(method: M, path: P, ops: UseAPIOptions = {}) {
+    const [params, setParams] = useState<Req>(null as Req)
     const { i18n } = useTranslation()
+    const { headers, enabled, staleTime = Infinity, gcTime = 5 * 60 * 1000 } = ops
 
     const q = useQuery<Res>({
         queryKey: [params, i18n.resolvedLanguage],
@@ -28,12 +37,17 @@ export function useAPI<
                 method,
                 url: path,
                 params,
+                data: params,
+                headers: headers || {},
             })
 
             return data
         },
         initialData: null as Res,
-        enabled: params !== null,
+        retry: 1,
+        staleTime,
+        gcTime,
+        enabled: enabled === true || params !== null,
     })
 
     return [setParams, q] as const
