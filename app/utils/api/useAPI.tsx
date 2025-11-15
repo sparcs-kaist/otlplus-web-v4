e53ed81e80882c4f74252581956a5e233274ed81
yaong12 <yaong@sparcs.org>
2025-11-16 02:34:50 +0900

    type verification on useAPI

--- app/utils/api/useAPI.tsx
+++ app/utils/api/useAPI.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react"
+import { type Dispatch, type SetStateAction, useState } from "react"
 
 import { type UseMutationOptions, useMutation, useQuery } from "@tanstack/react-query"
+import { type UseMutationResult, type UseQueryResult } from "@tanstack/react-query"
 import { type AxiosHeaders } from "axios"
 import { useTranslation } from "react-i18next"
 
@@ -15,20 +16,27 @@ import {
 } from "./getAPIType"
 
 type UseAPIQueryOptions = {
+    headers?: AxiosHeaders
     enabled?: boolean
     staleTime?: number
     gcTime?: number
 }
+
+type Merge<A, B> = [B] extends [never] ? A : A & B
 
 type UseAPIMutationOptions<Res, Req> = Omit<
     UseMutationOptions<Res, Error, Req, unknown>,
     "mutationFn"
 >
 
-type UseAPIOptions<Res, Req> = {
-    headers?: AxiosHeaders
-} & UseAPIQueryOptions &
-    UseAPIMutationOptions<Res, Req>
+type UseAPIOptions<M, Req, Res> = Merge<
+    UseAPIQueryOptions,
+    M extends "GET" ? never : UseAPIMutationOptions<Res, Req>
+>
+
+type UseAPIReturn<M, Req, Res> = M extends "GET"
+    ? [UseQueryResult<Res, Error>, Dispatch<SetStateAction<Req>>]
+    : [UseMutationResult<Res, Error, Req, unknown>, (params: Req) => void]
 
 export function useAPI<
     M extends Method<GetOriginalPath<P>>,
@@ -41,11 +49,15 @@ export function useAPI<
         M,
         keyof ResponseMap[GetOriginalPath<P>]
     >],
->(method: M, path: P, ops: UseAPIOptions<Res, Req> = {}) {
+>(
+    method: M,
+    path: P,
+    ops: UseAPIOptions<M, Req, Res> = {} as UseAPIOptions<M, Req, Res>,
+): UseAPIReturn<M, Req, Res> {
     const { i18n } = useTranslation()
     const {
-        headers,
-        enabled,
+        headers = {},
+        enabled = false,
         staleTime = Infinity,
         gcTime = 5 * 60 * 1000,
         ...mutationOps
@@ -57,35 +69,40 @@ export function useAPI<
             queryKey: [path, params, i18n.resolvedLanguage],
             queryFn: async () => {
                 const { data } = await axiosClient.request<Res>({
-                    method: method,
+                    method,
                     url: path,
-                    params: params,
-                    headers: headers || {},
+                    params,
+                    headers,
                 })
 
                 return data
             },
             retry: 1,
-            staleTime: staleTime,
-            gcTime: gcTime,
+            staleTime,
+            gcTime,
             enabled: enabled || params !== null,
         })
-        return [query, setParams] as const
+
+        return [query, setParams] as unknown as UseAPIReturn<M, Req, Res>
+    } else {
+        const mutation = useMutation<Res, Error, Req>({
+            mutationFn: async (params: Req) => {
+                const { data } = await axiosClient.request<Res>({
+                    method: method,
+                    url: path,
+                    data: ["POST", "PUT", "PATCH"].includes(method) ? params : undefined,
+                    params: ["DELETE"].includes(method) ? params : undefined,
+                    headers: headers || {},
+                })
+                return data
+            },
+            ...mutationOps,
+        })
+
+        function recallFunction(params: Req) {
+            mutation.mutate(params, mutationOps)
+        }
+
+        return [mutation, recallFunction] as UseAPIReturn<M, Req, Res>
     }
-
-    const mutation = useMutation<Res, Error, Req>({
-        mutationFn: async (params: Req) => {
-            const { data } = await axiosClient.request<Res>({
-                method: method,
-                url: path,
-                data: ["POST", "PUT", "PATCH"].includes(method) ? params : undefined,
-                params: ["DELETE"].includes(method) ? params : undefined,
-                headers: headers || {},
-            })
-            return data
-        },
-        ...mutationOps,
-    })
-
-    return [mutation.mutate, mutation] as const
 }
