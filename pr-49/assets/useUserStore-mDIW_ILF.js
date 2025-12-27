1aae28a805369a57f00af74e26b72f105537356f
happycastle114 <happycastle114@users.noreply.github.com>
2025-12-27 15:02:02 +0000

    deploy: e4be804eade798c8501a2d33ef16766f05c1eafd

---
+++ pr-49/assets/useUserStore-mDIW_ILF.js
@@ -0,0 +1,1 @@
+import{w as r}from"./useIsDevice-C631H4qQ.js";const u=r(s=>({user:null,status:"loading",setUser:e=>s({user:e,status:"success"}),clearUser:()=>s({user:null,status:"idle"})}));export{u};
