1cbe7c0f08d543219a3e8608807250f6b71b1f92
MBP16 <MBP16@users.noreply.github.com>
2025-12-27 19:16:42 +0000

    deploy: fe3c089de9b83e96db88be344d0105ecc30b5f8b

---
+++ pr-56/assets/useUserStore-DuKn1vpD.js
@@ -0,0 +1,2 @@
+import{v as r}from"./useIsDevice-y69o2Tr-.js";const u=r(s=>({user:null,status:"loading",setUser:e=>s({user:e,status:"success"}),clearUser:()=>s({user:null,status:"idle"})}));export{u};
+//# sourceMappingURL=useUserStore-DuKn1vpD.js.map
