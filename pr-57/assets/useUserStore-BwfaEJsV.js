2b61b7d78286698ec8366f1f84c2b724dc63bcb1
happycastle114 <happycastle114@users.noreply.github.com>
2025-12-27 19:38:08 +0000

    deploy: 3232a9e4a3077fb607549d47d21ff45f69fb75cc

---
+++ pr-57/assets/useUserStore-BwfaEJsV.js
@@ -0,0 +1,2 @@
+import{c as r}from"./index-CtIsX8Fj.js";const u=r(s=>({user:null,status:"loading",setUser:e=>s({user:e,status:"success"}),clearUser:()=>s({user:null,status:"idle"})}));export{u};
+//# sourceMappingURL=useUserStore-BwfaEJsV.js.map
