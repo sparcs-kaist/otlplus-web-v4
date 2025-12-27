8b1825d444950375bac211aa6d783fd99799d812
happycastle114 <happycastle114@users.noreply.github.com>
2025-12-27 16:01:33 +0000

    deploy: 848de34cd140f8344f79ed52902a464060a59d87

---
+++ pr-52/assets/useUserStore-DTqUAWQp.js
@@ -0,0 +1,2 @@
+import{v as r}from"./useIsDevice-BKakq9Kz.js";const u=r(s=>({user:null,status:"loading",setUser:e=>s({user:e,status:"success"}),clearUser:()=>s({user:null,status:"idle"})}));export{u};
+//# sourceMappingURL=useUserStore-DTqUAWQp.js.map
