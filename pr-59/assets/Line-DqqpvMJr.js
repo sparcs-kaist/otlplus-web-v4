c10cf80ea3672d8ebe95a8bfd5608785e5d9e8eb
MBP16 <MBP16@users.noreply.github.com>
2025-12-27 20:21:07 +0000

    deploy: 350cda5ae0c4cebf425a3a5a164d506124dd4857

---
+++ pr-59/assets/Line-DqqpvMJr.js
@@ -0,0 +1,6 @@
+import{j as r}from"./emotion-react-jsx-runtime.browser.esm-Cql-SmI8.js";import"./chunk-UIGDSWPH-ClC-Bows.js";import{n as s}from"./FlexWrapper-B3Tmr1Ow.js";const c=(e,o)=>{if(typeof o=="string"&&o.includes(".")){const[n,t]=o.split("."),i=e.colors[n];if(typeof i=="object"&&t&&t in i)return i[t]}return e.colors[o]},f=s.div`
+    width: 100%;
+    height: ${e=>e.height}px;
+    background: ${({theme:e,color:o})=>c(e,o)};
+`;function d({height:e=1,color:o="Line.default"}){return r(f,{height:e,color:o})}export{d as L};
+//# sourceMappingURL=Line-DqqpvMJr.js.map
