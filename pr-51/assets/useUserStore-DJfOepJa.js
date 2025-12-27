7110a9f74de9eaf11edb1b4db629f9405f419818
happycastle114 <happycastle114@users.noreply.github.com>
2025-12-27 15:36:04 +0000

    deploy: e48f3bab888eba06e1f6991ab19cd9f970a3ff85

---
+++ pr-51/assets/useUserStore-DJfOepJa.js
@@ -0,0 +1,1 @@
+import{w as r}from"./useIsDevice-CGASXFcU.js";const u=r(s=>({user:null,status:"loading",setUser:e=>s({user:e,status:"success"}),clearUser:()=>s({user:null,status:"idle"})}));export{u};
