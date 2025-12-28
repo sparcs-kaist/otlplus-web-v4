c305fd574b45f2341583066f4edddc4963f47540
mbp16 <phw1508@naver.com>
2025-12-28 17:56:19 +0900

    feat: prevent self like in review

--- app/features/write-reviews/sections/ReviewRightSection/LikedReviewsSubSection.tsx
+++ app/features/write-reviews/sections/ReviewRightSection/LikedReviewsSubSection.tsx
@@ -1,40 +1,26 @@
 import { useEffect } from "react"
 
 import { useTranslation } from "react-i18next"
-import { useInView } from "react-intersection-observer"
 
 import LoadingCircle from "@/common/components/LoadingCircle"
 import ReviewBlock from "@/common/components/reviews/ReviewBlock"
 import FlexWrapper from "@/common/primitives/FlexWrapper"
 import Typography from "@/common/primitives/Typography"
-import { useInfiniteAPI } from "@/utils/api/useInfiniteAPI"
+import { useAPI } from "@/utils/api/useAPI"
 import useUserStore from "@/utils/zustand/useUserStore"
 
 function LikedReviewsSection() {
     const { t } = useTranslation()
     const { user, status } = useUserStore()
 
-    const { query, setParams, data } = useInfiniteAPI(
-        "GET",
-        `/users/${user?.id}/reviews/liked`,
-        {
-            enabled: status === "success",
-            infinites: ["reviews"],
-            limit: 10,
-        },
-    )
-
-    const { ref, inView } = useInView({ threshold: 0 })
-
-    useEffect(() => {
-        if (inView && query.hasNextPage && !query.isFetchingNextPage)
-            query.fetchNextPage()
-    }, [inView, query])
+    const { query, setParams } = useAPI("GET", `/users/${user?.id}/reviews/liked`, {
+        enabled: status === "success",
+    })
 
     useEffect(() => {
         if (user === null) return
         setParams({ userId: user.id })
-    }, [])
+    }, [status])
 
     return (
         <FlexWrapper direction="column" align="stretch" gap={12}>
@@ -47,11 +33,9 @@ function LikedReviewsSection() {
                 <LoadingCircle />
             ) : (
                 <FlexWrapper direction="column" align="stretch" gap={12}>
-                    {data?.reviews.map((review) => (
+                    {query.data?.reviews.map((review) => (
                         <ReviewBlock review={review} key={review.id} />
                     ))}
-
-                    {query.hasNextPage && <LoadingCircle ref={ref} />}
                 </FlexWrapper>
             )}
         </FlexWrapper>
