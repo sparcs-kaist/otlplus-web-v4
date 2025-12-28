6aa7a76fcac5d1977ebdc601151de6101fdc9ff2
yaong12 <yaong12@users.noreply.github.com>
2025-12-28 03:15:09 +0000

    deploy: add804465ba0fef602f7e5c6e1ef650a4a1568b4

---
+++ pr-61/assets/useUserStore-DgZDi1i-.js
@@ -0,0 +1,2 @@
+import{c as r}from"./index-jZBtGqAs.js";const u=r(s=>({user:null,status:"loading",setUser:e=>s({user:e,status:"success"}),clearUser:()=>s({user:null,status:"idle"})}));export{u};
+//# sourceMappingURL=useUserStore-DgZDi1i-.js.map
