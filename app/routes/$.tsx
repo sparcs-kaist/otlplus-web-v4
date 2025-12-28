5a3d4612187693791e29b837f1a33c60c4f5c796
yaong12 <yaong@sparcs.org>
2025-12-29 01:28:19 +0900

    feat: implemented 404 error handling page

---
+++ app/routes/$.tsx
@@ -0,0 +1,22 @@
+import FlexWrapper from "@/common/primitives/FlexWrapper"
+import Typography from "@/common/primitives/Typography"
+
+export default function Page404() {
+    return (
+        <FlexWrapper
+            direction="row"
+            align="stretch"
+            justify="center"
+            gap={0}
+            flex="1 0 0"
+        >
+            <FlexWrapper
+                direction="column"
+                align="center"
+                justify="center"
+                gap={16}
+                flex="1 0 0"
+            ></FlexWrapper>
+        </FlexWrapper>
+    )
+}
