e700089ce6cab89ad27857495dcf3f4f44f464e1
yaong12 <yaong@sparcs.org>
2025-12-29 03:28:21 +0900

    fix: fixed review double fetching error

--- app/features/write-reviews/sections/ReviewRightSection/RecentFeedSubSection.tsx
+++ app/features/write-reviews/sections/ReviewRightSection/RecentFeedSubSection.tsx
@@ -25,7 +25,7 @@ function RecentFeedSubSection() {
     useEffect(() => {
         if (inView && query.hasNextPage && !query.isFetchingNextPage)
             query.fetchNextPage()
-    }, [inView, query])
+    }, [inView])
 
     useEffect(() => {
         setParams({
