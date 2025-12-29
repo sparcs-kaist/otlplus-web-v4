13bd400f291f8d8ff40173edea0b57a4daeee260
yaong12 <yaong12@users.noreply.github.com>
2025-12-29 17:33:40 +0000

    deploy: dda21cdf1672f82c191cad15b666aa29be24d826

---
+++ pr-74/assets/useUserStore-CMO0pyiB.js
@@ -0,0 +1,2 @@
+import{c as r}from"./index-CHd--ufo.js";const u=r(s=>({user:null,status:"loading",setUser:e=>s({user:e,status:"success"}),clearUser:()=>s({user:null,status:"idle"})}));export{u};
+//# sourceMappingURL=useUserStore-CMO0pyiB.js.map
