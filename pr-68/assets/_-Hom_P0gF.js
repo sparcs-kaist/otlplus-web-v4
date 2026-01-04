910db6f7d0af78cf5a2e0be847c102c57ac0e126
happycastle114 <happycastle114@users.noreply.github.com>
2026-01-04 12:58:47 +0000

    deploy: b919f04abe28cd08697f957f810c4075cd5b4eb4

---
+++ pr-68/assets/_-Hom_P0gF.js
@@ -0,0 +1,30 @@
+import{w as d,y as m}from"./chunk-UIGDSWPH-ClhI_Z4l.js";import{j as o,a as r}from"./emotion-react-jsx-runtime.browser.esm-D2PZwVTC.js";import{n as t,F as n}from"./FlexWrapper-B6Hxm4Zw.js";import{m as p,B as s}from"./media-MLnbxw48.js";import{T as e}from"./Typography-DeI78L6T.js";import{u as g}from"./useTranslation-DrmvPmDd.js";const h=t.div`
+    width: 100%;
+    min-height: calc(100vh - 60px);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 40px 20px;
+    box-sizing: border-box;
+    background-color: ${({theme:a})=>a.colors.Background.Page.default};
+`,u=t(n)`
+    max-width: 400px;
+    width: 100%;
+`,x=t.img`
+    width: 120px;
+    height: auto;
+    margin-bottom: 16px;
+
+    ${p.mobile} {
+        width: 100px;
+    }
+`,f=t(e)`
+    text-align: center;
+    max-width: 320px;
+    line-height: 1.5;
+`,w=t(n)`
+    width: 100%;
+    max-width: 280px;
+`,v=d(function(){const{t:i}=g(),c=m(),l=()=>{c("/")};return o(h,{children:r(u,{direction:"column",align:"center",justify:"center",gap:32,children:[r(n,{direction:"column",align:"center",gap:16,children:[o(x,{src:"/otlplus-web-v4/pr-68/headerIcon.png",alt:"OTL Logo"}),r(n,{direction:"column",align:"center",gap:8,children:[o(e,{type:"BiggerBold",color:"Highlight.default",children:"404"}),o(e,{type:"Big",color:"Text.dark",children:i("common.notFound.title")})]}),o(f,{type:"Normal",color:"Text.light",children:i("common.notFound.description")})]}),o(w,{direction:"column",align:"center",gap:12,children:o(s,{type:"default",onClick:l,$isFlexRow:!0,children:o(e,{type:"Normal",children:i("common.notFound.goHome")})})})]})})});export{v as default};
+//# sourceMappingURL=_-Hom_P0gF.js.map
