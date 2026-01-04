f88a2990261053896ce7aa8f182a8b9b31a15e94
Hyunwoo Park <phw1508@naver.com>
2026-01-05 06:00:38 +0900

    feat: add my timetable lecture warning (#75)

--- app/i18n/locales/en/timetable.ts
+++ app/i18n/locales/en/timetable.ts
@@ -2,6 +2,7 @@ export const timetable = {
     myTimetable: "My Timetable",
     examTimetable: "Exam Timetable",
     addLectureConflict: "Cannot add lectures that conflict with existing timetable.",
+    myTimeTableLectureAddWarning: "Cannot add lectures to My Timetable.",
     copyImage: "Copy as Image",
     exportImage: "Export as Image",
     exportICal: "Export as Calendar",
