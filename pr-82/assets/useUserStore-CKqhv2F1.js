dd19783985ebc0dcd28f66be859f64ff7cd76b93
happycastle114 <happycastle114@users.noreply.github.com>
2026-01-18 13:07:28 +0000

    deploy: eb45223d78018a2387a72995694daed268639cb4

---
+++ pr-82/assets/useUserStore-CKqhv2F1.js
@@ -0,0 +1,2 @@
+import{c as r}from"./index-CPg-wSkd.js";const u=r(s=>({user:null,status:"loading",setUser:e=>s({user:e,status:"success"}),clearUser:()=>s({user:null,status:"idle"})}));export{u};
+//# sourceMappingURL=useUserStore-CKqhv2F1.js.map
