dcdfbef4e303ee5bf9cb37482d87f93112abd86a
MBP16 <MBP16@users.noreply.github.com>
2026-02-19 15:45:12 +0000

    deploy: b2612a55135243824c50f54aee077e8ceb05bf20

---
+++ pr-84/assets/Line-Dy6wFX6q.js
@@ -0,0 +1,6 @@
+import{j as i}from"./emotion-react-jsx-runtime.browser.esm-Br5a-SdJ.js";import"./chunk-EPOLDU6W-BJizBTXA.js";import{n as f}from"./FlexWrapper-B1PfKErF.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"b2612a55135243824c50f54aee077e8ceb05bf20"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="4dcceb52-c108-4c07-a919-01bda5c6406d",e._sentryDebugIdIdentifier="sentry-dbid-4dcceb52-c108-4c07-a919-01bda5c6406d")})()}catch{}const s=(e,n)=>{if(typeof n=="string"&&n.includes(".")){const[t,o]=n.split("."),d=e.colors[t];if(typeof d=="object"&&o&&o in d)return d[o]}return e.colors[n]},c=f.div`
+    width: 100%;
+    height: ${e=>e.height}px;
+    background: ${({theme:e,color:n})=>s(e,n)};
+`;function b({height:e=1,color:n="Line.default"}){return i(c,{height:e,color:n})}export{b as L};
+//# sourceMappingURL=Line-Dy6wFX6q.js.map
---
+++ pr-84/assets/license-Cws2eJTT.js
@@ -0,0 +1,9 @@
+import{w as i}from"./chunk-EPOLDU6W-BJizBTXA.js";import{j as t,a as o}from"./emotion-react-jsx-runtime.browser.esm-Br5a-SdJ.js";import{n as r,F as a}from"./FlexWrapper-B1PfKErF.js";import{T as d}from"./Typography-kb2q0Sls.js";import{W as l}from"./Widget-BdcQMHqN.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"b2612a55135243824c50f54aee077e8ceb05bf20"}}catch{}})();try{(function(){var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{},n=new e.Error().stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="9ad95bb2-3a8a-4548-843d-4990392d8e90",e._sentryDebugIdIdentifier="sentry-dbid-9ad95bb2-3a8a-4548-843d-4990392d8e90")})()}catch{}const s=r(l)`
+    width: 100%;
+    max-width: 1000px;
+    padding: 20px;
+    align-items: center;
+    height: 100%;
+    flex-grow: 1;
+`,u=i(function(){return t(a,{direction:"column",gap:0,align:"center",padding:"20px",style:{minHeight:"calc(100vh - 60px)"},children:o(s,{direction:"column",gap:20,children:[t(d,{color:"Text.default",type:"BiggerBold",children:"Licenses"}),t(d,{color:"Text.default",type:"Normal",children:`Copyright © 2016-${new Date().getFullYear()}, SPARCS OTL Team. All rights reserved.`})]})})});export{u as default};
+//# sourceMappingURL=license-Cws2eJTT.js.map
