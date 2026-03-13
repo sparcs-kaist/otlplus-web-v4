7617dae085b6de878bd8c024b9acf917b7708655
yaong12 <yaong12@users.noreply.github.com>
2026-03-13 13:07:38 +0000

    deploy: 8a807888b269277614da5f4132c16623ed44ae99

---
+++ pr-141/assets/StyledDivider-irWfkA0E.js
@@ -0,0 +1,10 @@
+import{u as n,j as t}from"./emotion-react-jsx-runtime.browser.esm-DGIfoTEP.js";import{s as r}from"./FlexWrapper-CdxoGRQv.js";(function(){try{var e=typeof window<"u"?window:typeof global<"u"?global:typeof globalThis<"u"?globalThis:typeof self<"u"?self:{};e.SENTRY_RELEASE={id:"8a807888b269277614da5f4132c16623ed44ae99"};var d=new e.Error().stack;d&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[d]="14bb4143-3edd-4231-a5a3-8d050a6303cc",e._sentryDebugIdIdentifier="sentry-dbid-14bb4143-3edd-4231-a5a3-8d050a6303cc")}catch{}})();const o=r.div`
+    width: 100%;
+    height: 1px;
+    background: ${({color:e})=>e};
+`,s=r.div`
+    width: 1px;
+    background: ${({color:e})=>e};
+    align-self: stretch;
+`,c=({direction:e="row",color:d})=>{const i=n();return e=="row"?t(o,{color:d||i.colors.Line.divider}):t(s,{color:d||i.colors.Line.divider})};export{c as S};
+//# sourceMappingURL=StyledDivider-irWfkA0E.js.map
