255acbb7dad800a8f09d77db139150dbd2828832
MBP16 <MBP16@users.noreply.github.com>
2026-08-10 14:46:27 +0000

    deploy: 180e2ac1f2ba48c434a899b95a7da45cc972d555

---
+++ pr-152/assets/_-DtNgiaxx.js
@@ -0,0 +1,30 @@
+(function(){try{var e=typeof window<`u`?window:typeof global<`u`?global:typeof globalThis<`u`?globalThis:typeof self<`u`?self:{};e.SENTRY_RELEASE={id:`180e2ac1f2ba48c434a899b95a7da45cc972d555`};var t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]=`af7e1153-6a9e-4ecd-a890-5d53ed1f9f8d`,e._sentryDebugIdIdentifier=`sentry-dbid-af7e1153-6a9e-4ecd-a890-5d53ed1f9f8d`)}catch{}})();import{A as e,O as t}from"./chunk-QFMPRPBF-DreaWL4j.js";import{n,r}from"./emotion-react-jsx-runtime.browser.esm-D-I6T8We.js";import{n as i,t as a}from"./FlexWrapper-BV9gBHyN.js";import{t as o}from"./useTranslation-BxCUh1F0.js";import{t as s}from"./Typography-DhLyEEgG.js";import{t as c}from"./Button-p4jf3hpg.js";import{t as l}from"./media-BXr711Z1.js";var u=i.div`
+    width: 100%;
+    min-height: calc(100vh - 60px);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 40px 20px;
+    box-sizing: border-box;
+    background-color: ${({theme:e})=>e.colors.Background.Page.default};
+`,d=i(a)`
+    max-width: 400px;
+    width: 100%;
+`,f=i.img`
+    width: 120px;
+    height: auto;
+    margin-bottom: 16px;
+
+    ${l.mobile} {
+        width: 100px;
+    }
+`,p=i(s)`
+    text-align: center;
+    max-width: 320px;
+    line-height: 1.5;
+`,m=i(a)`
+    width: 100%;
+    max-width: 280px;
+`,h=e(function(){let{t:e}=o(),i=t();return n(u,{children:r(d,{direction:`column`,align:`center`,justify:`center`,gap:32,children:[r(a,{direction:`column`,align:`center`,gap:16,children:[n(f,{src:`/otlplus-web-v4/pr-152/headerIcon.png`,alt:`OTL Logo`}),r(a,{direction:`column`,align:`center`,gap:8,children:[n(s,{type:`BiggerBold`,color:`Highlight.default`,children:`404`}),n(s,{type:`Big`,color:`Text.dark`,children:e(`common.notFound.title`)})]}),n(p,{type:`Normal`,color:`Text.light`,children:e(`common.notFound.description`)})]}),n(m,{direction:`column`,align:`center`,gap:12,children:n(c,{type:`default`,onClick:()=>{i(`/`)},$isFlexRow:!0,children:n(s,{type:`Normal`,children:e(`common.notFound.goHome`)})})})]})})});export{h as default};
+//# sourceMappingURL=_-DtNgiaxx.js.map
