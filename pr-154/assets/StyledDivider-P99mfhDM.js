543f88789613769d7b155abbc43e95b27ed6f136
happycastle114 <happycastle114@users.noreply.github.com>
2026-08-12 05:25:09 +0000

    deploy: dd77648980900e748a26ea903da6ee4c0258a96e

---
+++ pr-154/assets/StyledDivider-P99mfhDM.js
@@ -0,0 +1,10 @@
+(function(){try{var e=typeof window<`u`?window:typeof global<`u`?global:typeof globalThis<`u`?globalThis:typeof self<`u`?self:{};e.SENTRY_RELEASE={id:`dd77648980900e748a26ea903da6ee4c0258a96e`};var t=new e.Error().stack;t&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[t]=`cb216a0f-eb36-4f1f-955b-fa61f96ff928`,e._sentryDebugIdIdentifier=`sentry-dbid-cb216a0f-eb36-4f1f-955b-fa61f96ff928`)}catch{}})();import{l as e,n as t}from"./emotion-react-jsx-runtime.browser.esm-BJoXzW6-.js";import{n}from"./FlexWrapper-Myr8cWKN.js";var r=n.div`
+    width: 100%;
+    height: 1px;
+    background: ${({color:e})=>e};
+`,i=n.div`
+    width: 1px;
+    background: ${({color:e})=>e};
+    align-self: stretch;
+`,a=({direction:n=`row`,color:a})=>{let o=e();return t(n==`row`?r:i,{color:a||o.colors.Line.divider})};export{a as t};
+//# sourceMappingURL=StyledDivider-P99mfhDM.js.map
