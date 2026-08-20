c9132aee4e66d01cd06a2229ec6720802c0617c7
Soungmin <soungmin@Soungminui-MacBookPro.local>
2026-08-20 13:33:00 +0900

    refactor: type local storage keys

--- app/utils/localStorage.ts
+++ app/utils/localStorage.ts
@@ -1,16 +1,18 @@
+import type { LocalStorageKey } from "@/utils/storageKeys"
+
 // WARNING: MUST BE CALLED IN A BROWSER ENVIRONMENT
 
 const LOCAL_STORAGE_SET_EVENT = "local-storage-set"
 
-export const getLocalStorageItem = (key: string) =>
+export const getLocalStorageItem = (key: LocalStorageKey) =>
     typeof window !== "undefined" ? localStorage.getItem(key) : null
 
-export const setLocalStorageItem = (key: string, value: string) => {
+export const setLocalStorageItem = (key: LocalStorageKey, value: string) => {
     localStorage.setItem(key, value)
     window.dispatchEvent(new Event(LOCAL_STORAGE_SET_EVENT))
 }
 
-export const removeLocalStorageItem = (key: string) => {
+export const removeLocalStorageItem = (key: LocalStorageKey) => {
     localStorage.removeItem(key)
     window.dispatchEvent(new Event(LOCAL_STORAGE_SET_EVENT))
 }
