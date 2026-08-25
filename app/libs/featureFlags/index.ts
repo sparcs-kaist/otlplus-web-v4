e10e94e6cd0a69064bc6628e5f89501a4f15e976
Soungmin <soungmin@Soungminui-MacBookPro.local>
2026-08-25 12:30:51 +0900

    refactor(flags): trim barrel to public api

--- app/libs/featureFlags/index.ts
+++ app/libs/featureFlags/index.ts
@@ -1,4 +1,3 @@
 export { FLAGS, resolveFlag, type FlagKey, type FlagSource } from "./core"
-export { parseFlagEnvDefaults, getFlagEnvDefault } from "./envDefaults"
 export { useFeatureFlag, type FeatureFlagState } from "./useFeatureFlag"
 export { RequireFeatureFlag } from "./RequireFeatureFlag"
